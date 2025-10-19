2faeea57effff7da684a4a841bb2335376b6b009
matrzask <maciejtrzask@student.agh.edu.pl>
2025-10-19 19:03:24 +0200

    study deck switching cards

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Flashcard } from './components/flashcard/flashcard';
 import { Card } from './types/card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Flashcard],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
--- src/app/pages/learn-deck/study-deck.ts
+++ src/app/pages/learn-deck/study-deck.ts
@@ -13,19 +13,28 @@ import { ActivatedRoute } from '@angular/router';
 export class StudyDeck {
   flashcards: Card[] = [];
   deckId: string = '';
+  currentIndex: number = 0;
 
   constructor(private deckService: DeckService, private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.route.paramMap.subscribe((params) => {
       this.deckId = params.get('id') || '';
-      this.LoadFlashcards();
+      this.loadFlashcards();
     });
   }
 
-  LoadFlashcards() {
+  loadFlashcards() {
     this.deckService.getFlashcardsByDeckId(this.deckId).then((cards: Card[]) => {
       this.flashcards = cards;
     });
+  }
+
+  changeCard(dir: number) {
+    const newIndex = this.currentIndex + dir;
+    if (newIndex >= 0 && newIndex < this.flashcards.length) {
+      this.currentIndex = newIndex;
+    }
+    console.log('Current Index:', this.currentIndex);
   }
 }
